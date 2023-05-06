3bb5ee5136d16fade95ce8ab343ae12229fb7a2a
Bartek Pacia <barpac02@gmail.com>
2023-05-06 15:45:14 +0200

    run csharpier

--- Quiz/App.xaml.cs
+++ Quiz/App.xaml.cs
@@ -2,11 +2,11 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-		// test
-	}
+        MainPage = new AppShell();
+        // test
+    }
 }
