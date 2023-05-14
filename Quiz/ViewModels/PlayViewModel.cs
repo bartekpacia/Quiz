474c7d3082f840cd46de102baf930f63e847f9ef
Bartek Pacia <barpac02@gmail.com>
2023-05-15 00:04:04 +0200

    PlayPage: display question and answers

--- Quiz/ViewModels/PlayViewModel.cs
+++ Quiz/ViewModels/PlayViewModel.cs
@@ -3,18 +3,12 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Quiz.Models;
 
 namespace Quiz.ViewModels;
 
 public partial class PlayViewModel : ObservableObject
 {
-    // TODO: Quiz properties
-    //  * List of all quizes
-
-    // Current question
-
-
-
     const string goodAnswer = "Warsaw";
 
     public PlayViewModel()
@@ -25,6 +19,12 @@ public partial class PlayViewModel : ObservableObject
         answers.Add("Cracow");
         answers.Add("Gliwice");
         answers.Add("Gdańsk");
+
+        var currentQuizId = App.Store.currentQuizId;
+        Quiz = App.Db.Quizzes.Find(currentQuizId);
+        Questions = new ObservableCollection<Question>(Quiz.Questions);
+        CurrentQuestion = Questions.First();
+        CurrentAnswers = new ObservableCollection<Answer>(CurrentQuestion.Answers);
     }
 
     [ObservableProperty]
@@ -39,4 +39,16 @@ public partial class PlayViewModel : ObservableObject
         if (answer == goodAnswer) { }
         else { }
     }
+
+    [ObservableProperty]
+    public QuizModel quiz;
+
+    [ObservableProperty]
+    public ObservableCollection<Question> questions;
+
+    [ObservableProperty]
+    public Question currentQuestion;
+
+    [ObservableProperty]
+    public ObservableCollection<Answer> currentAnswers;
 }
